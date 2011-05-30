6366cea8b72a8769cd3cacc4d783a63cee9d0bba
pycmam <pycmam@gmail.com>
2011-05-30 19:28:21 +0600

    Initial

---
+++ modules/sfAdminDash/actions/actions.class.php
@@ -0,0 +1,15 @@
+<?php
+require_once(dirname(__FILE__).'/../lib/BasesfAdminDashActions.class.php');
+
+/**
+ * sfAdminDash actions.
+ *
+ * @package    plugins
+ * @subpackage sfAdminDash
+ * @author     kevin
+ * @version    SVN: $Id: actions.class.php 30505 2010-08-02 13:53:29Z Crafty_Shadow $
+ */ 
+class sfAdminDashActions extends BasesfAdminDashActions
+{
+  
+}
---
+++ modules/sfAdminDash/actions/components.class.php
@@ -0,0 +1,14 @@
+<?php
+require_once(dirname(__FILE__).'/../lib/BasesfAdminDashComponents.class.php');
+
+/**
+ * sfAdminDash components.
+ *
+ * @package    plugins
+ * @subpackage sfAdminDash
+ * @author     kevin
+ * @version    SVN: $Id: components.class.php 30505 2010-08-02 13:53:29Z Crafty_Shadow $
+ */ 
+class sfAdminDashComponents extends BasesfAdminDashComponents
+{
+}
